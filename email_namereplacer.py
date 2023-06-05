463e825b4ab3b2e9e7bd897fc1f854ad29c9a58b
Bravo <bravo6here@gmail.com>
2023-06-05 17:04:33 +0200

    It works!!!

--- email_namereplacer.py
+++ email_namereplacer.py
@@ -1,7 +1,18 @@
 filepath_names = R"C:\Users\Jan - Hall 3000\Desktop\Pythonus\Python web scrapping\Email-namereplace1\Input\Names.txt"
 filepath_email = R"C:\Users\Jan - Hall 3000\Desktop\Pythonus\Python web scrapping\Email-namereplace1\Input\letter.txt"
+output_path = R"C:\Users\Jan - Hall 3000\Desktop\Pythonus\Python web scrapping\Email-namereplace1\Output"
+output_path += R"\ "
+sender_name = "Bravo"
+
 with open(f"{filepath_names}", "r") as namefile:
     names = namefile.readlines()
 with open(f"{filepath_email}", "r") as namefile:
-    email = namefile.read()
-    print(email)
+    email_content = namefile.read()
+
+for name in names:
+    name = name.strip()
+    email = email_content
+    email = email.replace("[name]", name)
+    email = email.replace("[sendersname]", sender_name)
+    with open(f"{output_path}{name}_email.txt", "w") as a:
+        a.write(f"{email}")
